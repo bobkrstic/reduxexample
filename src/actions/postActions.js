6f35b8976df9cba07563809a74508668b57c2182
Bob Krstic <slobodankrsticnis@yahoo.com>
2020-02-23 18:32:22 -0800

    BP completed

---
+++ src/actions/postActions.js
@@ -0,0 +1,27 @@
+import { FETCH_POSTS, NEW_POST } from "./types";
+
+// export function fetchPosts() {
+//   return function(dispatch) {
+
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//       .then(res => res.json())
+//       .then(posts =>
+//         dispatch({
+//           type: FETCH_POSTS,
+//           payload: posts
+//         })
+//       );
+//   };
+// }
+
+export const fetchPosts = () => dispatch => {
+  // console.log("fetching");
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(posts =>
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      })
+    );
+};
