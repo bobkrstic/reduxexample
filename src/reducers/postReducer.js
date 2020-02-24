ce1119355cff41ece885cb1114e600eafcfaff68
Bob Krstic <slobodankrsticnis@yahoo.com>
2020-02-23 18:07:57 -0800

    Setting up BP

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,14 @@
+// we will have actions here
+import { FETCH_POSTS, NEW_POST } from "../actions/types";
+
+const initialState = {
+  items: [],
+  item: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
